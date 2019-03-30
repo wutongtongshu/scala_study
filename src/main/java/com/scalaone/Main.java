161f02fbc61189cbf0eb2012379924457b6799f4
361440262@qq.com <361440262@qq.com>
2019-03-30 10:31:34 +0800

    scala + maven + idea 环境搭建

---
+++ src/main/java/com/scalaone/Main.java
@@ -0,0 +1,10 @@
+package com.scalaone;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.main(null);
+    }
+}
---
+++ src/main/scala/com/scalaone/HelloWorld.scala
@@ -0,0 +1,7 @@
+package com.scalaone
+
+class HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("hello scala");
+  }
+}
